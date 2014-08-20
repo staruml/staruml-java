6545f5155f454feb4cd03c5de2c247d8f7f112ef
이민규 <niklaus.lee@gmail.com>
2014-08-21 02:03:17 +0900

    Go back to previous API

--- JavaReverseEngineer.js
+++ JavaReverseEngineer.js
@@ -28,7 +28,7 @@ define(function (require, exports, module) {
 
     var Core            = staruml.getModule("core/Core"),
         Repository      = staruml.getModule("engine/Repository"),
-        CommandManager  = staruml.getModule("command/CommandManager"),
+        CommandManager  = staruml.getModule("menu/CommandManager"),
         UML             = staruml.getModule("uml/UML"),
         FileSystem      = staruml.getModule("filesystem/FileSystem"),
         FileSystemError = staruml.getModule("filesystem/FileSystemError"),
